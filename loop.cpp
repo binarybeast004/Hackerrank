5966a508b6b29ec40ad27417d0632747e18b7e08
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-09-30 23:33:31 +0600

    Create loop.cpp

---
+++ loop.cpp
@@ -0,0 +1,29 @@
+-----------------------------------------------------------------------
+|                          Assalamualaikum                            |
+|                                                                     |
+|                          Binarybeast004                             |
+-----------------------------------------------------------------------
+                       
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    
+    string M[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+    int a,b,i;
+    cin>> a >> b;
+    for(i=a; i<=b;i++){
+        if(i>9){
+            if(i%2==0){
+                cout<<"even"<<endl;
+            }
+            else {
+                cout<<"odd"<<endl;
+            }
+        }
+        else{
+            cout<<""<<M[i]<<endl;
+        }
+    }
+}
